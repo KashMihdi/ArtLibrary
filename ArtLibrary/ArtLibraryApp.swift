0f110c9cb045a4bc085b351471235a287de6ced1
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-11-06 08:44:03 +0300

    Initial Commit

---
+++ ArtLibrary/ArtLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtLibraryApp.swift
+//  ArtLibrary
+//
+//  Created by Kasharin Mikhail on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
